d85189aa635d85d0f11384dc9dbf217e2ea98429
gihyo-san <gihyo-san@example.com>
2026-07-29 00:37:38 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/zawzaw/worktime/WorkTimeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.zawzaw.worktime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTimeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkTimeManagerApplication.class, args);
+	}
+
+}
